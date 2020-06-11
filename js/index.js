3d4af5267cb021d6db88a0407c43668624d4325e
sofiaserrano <sofia.serrano1@decathlon.com>
2020-06-11 13:26:59 +0200

    feature/imageSponsorAnalysisWeb

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     $('#link-how-it-work').click(function(e) {
       e.preventDefault();
-      let sectionTop = $("#section-how-it-work").offset().top - 70;
+      let sectionTop = $("#section-how-it-work").offset().top - 55;
       $('html, body').animate({
         scrollTop: sectionTop
         }, 1000);
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     $('#link-sponsor').click(function(e) {
       e.preventDefault();
-      let sectionTop = $("#section-sponsor").offset().top - 70;
+      let sectionTop = $("#section-sponsor").offset().top - 55;
       $('html, body').animate({
         scrollTop: sectionTop
         }, 1000);
@@ -27,17 +27,47 @@ document.addEventListener("DOMContentLoaded", function() {
 
     $('#link-news').click(function(e) {
       e.preventDefault();
-      let sectionTop = $("#section-news").offset().top - 70;
+      let sectionTop = $("#section-news").offset().top - 55;   
+
+
+      $('html, body').animate({
+        scrollTop: sectionTop
+        }, 1000, function(){
+          $('.news-right').addClass('animated');
+          $('.news-right').addClass('bounceInRight');
+          $('.news-right').css('opacity','1');
+
+          $('.news-left').addClass('animated');
+          $('.news-left').addClass('bounceInLeft');
+          $('.news-left').css('opacity','1');        
+      });
+
+    });
+
+    $('#link-contact').click(function(e) {
+      e.preventDefault();
+      let sectionTop = $("#section-contact").offset().top - 55;
       $('html, body').animate({
         scrollTop: sectionTop
         }, 1000);
     });
 
-    $('#link-contact').click(function(e) {
-      e.preventDefault();
-      let sectionTop = $("#section-contact").offset().top - 70;
-      $('html, body').animate({
-        scrollTop: sectionTop
-        }, 1000);
-    });
+    
+
+    $(window).scroll(function() {
+      var top_of_element = $("#section-news").offset().top + 180;
+      var bottom_of_element = $("#section-news").offset().top + $("#section-news").outerHeight() + 180;
+      var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+      var top_of_screen = $(window).scrollTop();
+  
+      if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+        $('.news-right').addClass('animated');
+          $('.news-right').addClass('bounceInRight');
+          $('.news-right').css('opacity','1');
+
+          $('.news-left').addClass('animated');
+          $('.news-left').addClass('bounceInLeft');
+          $('.news-left').css('opacity','1');
+      }
+  });
 });
